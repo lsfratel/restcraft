27d12608890cb098720e637c67906ec41a418289
Lucas Santana <lsfratel@gmail.com>
2024-04-18 15:10:50 -0300

    chore: remove middlewares

--- tests/test_app/settings.py
+++ tests/test_app/settings.py
@@ -14,18 +14,9 @@ VIEWS = {
 }
 
 MIDDLEWARES = {
-    # 'restcraft.middleware.cors.CORSMiddleware',
     'test_app.middlewares.test_middleware',
 }
 
-CORS = {
-    'origin': ['http://localhost:8000'],
-    'headers': ['Content-Type', 'Authorization'],
-    'methods': ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    'exposed_headers': ['Content-Length', 'X-Foo', 'X-Bar'],
-    'credentials': True,
-    'max_age': 1200,
-}
 
 try:
     from .local_settings import *  # type: ignore # noqa: F403
